018e1b17e4f4980a9d6434832d9031b1af764db9
kdh <liardanc3@gmail.com>
2026-03-22 11:58:18 +0900

    React/SpringBoot 프로젝트 생성

---
+++ server/src/main/java/com/speedql/SpeedqlApplication.java
@@ -0,0 +1,13 @@
+package com.speedql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedqlApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/speedql/SpeedqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
